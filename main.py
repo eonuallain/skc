d30d3bf832be35e3ceb5e68fa2b25575bff62bd8
Eamonn O Nuallain <eamonn.o.nuallain@gmail.com>
2023-06-30 00:42:45 +0100

    kube config check

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import wx
 
+
+from kubeconfig import *
+
+if config_defined():
+    config_dict = get_config()
+    print(config_dict)
+else:
+    print('KUBECONFIG environment variable not set')
+    exit(1)
+
 app = wx.App()
 frame = wx.Frame(parent=None, title='SKC')
 frame.Centre()
