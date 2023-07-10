883ddf19f50ffce19528987d32aad4489beeeb74
Eamonn O Nuallain <eamonn.o.nuallain@gmail.com>
2023-07-10 01:09:17 +0100

    update console poc

--- src/console.py
+++ src/console.py
@@ -6,4 +6,17 @@ api_instance = client.CoreV1Api()
 node_list = api_instance.list_node()
 
 for node in node_list.items:
-	print(node.metadata.name)
+	node_name = node.metadata.name
+	labels = node.metadata.labels
+	status = api_instance.read_node_status(node_name)
+
+	print(f"Node\t: {node_name}")
+	#print(f"labels {labels}")
+	#print(f"status {status}")
+	#print(type(status))
+
+	conditions = status.status.conditions
+	#print(type(conditions))
+	for condition in conditions:
+		if "Ready" == condition.type:
+			print(f"Ready\t: {condition.status}")
